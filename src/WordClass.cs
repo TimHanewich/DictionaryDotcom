01d8b1d184bf51f9010e8f6363163463b3970666
Tim Hanewich <tahanewich@live.com>
2022-01-31 19:24:59 -0500

    Added WordClass enum

---
+++ src/WordClass.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DictionaryDotcom
+{
+    public enum WordClass
+    {
+        Noun = 0,
+        Verb = 1,
+        Adjective = 2,
+        Adverb = 3,
+        Pronoun = 4,
+        Preposition = 5,
+        Conjunction = 6,
+        Interjection = 7
+    }
+}
